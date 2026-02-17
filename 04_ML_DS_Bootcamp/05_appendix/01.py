1e7d012cbd54d5917177715a31ae033750916ec5
Michal Radomski <michalra@op.pl>
2026-02-17 23:08:54 +0100

    Machine Learning & Data Science Bootcamp - commit 54

---
+++ 04_ML_DS_Bootcamp/05_appendix/01.py
@@ -0,0 +1,35 @@
+# print("Hello World!")
+
+# age = input("How old are you? ")
+# print("You are: " + age + " years old!")
+
+# * Fundamental data types
+# int -> 3, 5, -2
+# float -> 0.5
+# complex
+# str
+# bool
+# list
+# tuple
+# set
+# dict
+# None
+
+# * Number and float
+print(type(2 + 4))
+print(type(2.5 + 4.5))
+print(2 + 4)
+print(type(2 - 4))
+print(type(2 * 4))
+print(2 / 4)
+print(2.1 + 4.9)  # 7.0 -> float
+print(2**4)
+print(10 // 4)  # -> integer rounded down: 2
+print(10 % 4)  # -> modulo: 2
+
+# * Math functions
+print(round(3.1))
+print(round(3.5))  # 4
+print(abs(0))
+print(abs(-3.1))
+print(abs(3.1))
