fdf6b7afe8b8b4f1f78fa70f2dd7780f10359e1f
Michal Radomski <michalra@op.pl>
2026-02-18 21:54:52 +0100

    Machine Learning & Data Science Bootcamp - commit 56

--- 04_ML_DS_Bootcamp/05_appendix/01.py
+++ 04_ML_DS_Bootcamp/05_appendix/01.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 # print("Hello World!")
 
 # age = input("How old are you? ")
@@ -80,3 +82,82 @@ print(long_string)
 print("hello" + " Michal")
 print("hello " + str(5))
 print("\n")
+
+# Type Conversion
+print(type(int(str(100))))
+
+a = str(100)
+b = int(a)
+c = type(b)
+print(c)
+print("\n")
+
+
+# Escape Sequences
+weather = '\t It\'s "kind of" sunny \n Hope you have a good day!'
+print(weather)
+print("\n")
+
+# Formatted Strings
+name = "Johnny"
+age = 55
+print("Hi" + name + ". You are" + str(age) + " years old.")
+print(f"Hi {name}. You are {age} years old.")  # f -> formatted string
+
+# Python2 Formatted Strings
+print("Hi {}. Your are {} years old.".format(name, age))
+print("Hi {1}. You are {0} years old.".format(age, name))
+print("Hi {new_name}. You are {age} years old.".format(new_name="Sally", age=100))
+print("\n")
+
+# String Indexes
+# [start:stop:stepover (default->1)]
+python = "I am PYTHON"
+print(python[1:4])  # " am"
+print(python[1:])  # " am PYTHON"
+print(python[:])  # "I am PYTHON"
+print(python[1:100])  # " am PYTHON"
+print(python[-1])  # "N"
+print(python[-4])  # " T"
+print(python[:-3])  # "I am PYT"
+print(python[-3:])  # "HON"
+print(python[::-1])  # "NOHTYP ma I"
+print("\n")
+
+# * Strings are Immutable!
+selfish = "01234567"
+selfish = selfish + "8"
+print(selfish)  # 012345678
+
+
+# Built-In Functions + Methods
+# https://docs.python.org/3/library/functions.html
+# https://www.w3schools.com/python/python_ref_string.asp
+print(len("Helllooooo"))
+quote = "to be or not to be"
+print(quote.upper())
+print(quote.capitalize())
+print(quote.find("be"))
+print(quote.replace("be", "me"))
+print(quote)  # "to be or not to be"
+
+
+# * Boolean
+is_cool = True
+is_cool = False
+print(bool(-0))  # False
+print(bool(0))  # False
+print(bool(1))  # True
+print(bool(0.5))  # True
+print(bool("0"))  # True
+print(bool("True"))  # True
+print(bool("False"))  # True
+print(bool(False))  # False
+print(bool("any random thing"))  # True
+
+# * Exercise
+current_year = datetime.now().year
+
+birth_year = input("What year were you born?\n")  # input -> type:string!
+age = current_year - int(birth_year)
+print(f"Your age is {age} years.")
