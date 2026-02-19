36cd1bb3354cbef4a275d1a42ce21e9339c931f9
Michal Radomski <michalra@op.pl>
2026-02-19 22:27:47 +0100

    Machine Learning & Data Science Bootcamp - commit 64

--- 04_ML_DS_Bootcamp/05_appendix/03.py
+++ 04_ML_DS_Bootcamp/05_appendix/03.py
@@ -68,3 +68,58 @@ is_friend = True
 is_user = True
 print(is_friend and is_user)  # True
 print(is_friend or is_user)  # True
+
+# * Logical Operators
+is_magician = False
+is_expert = True
+
+# Check if magician and expert: "you are a master magician"
+if is_magician and is_expert:
+    print("You are a master magician")  # ---
+
+# Check if magician but not expert: "at least you're getting there"
+elif is_magician and not is_expert:
+    print("At least you're getting there.")  # ---
+
+# Check if not a magician: "You need magic powers"
+elif not is_magician:
+    print("You need magic powers.")  # You need magic powers
+else:
+    print("Nothing")  # ---
+
+print("a" > "A")  # True -> ord('a') > ord('A')
+print(
+    ord("a")
+)  # Outputs 97 -> integer representing the Unicode code point of that character.
+print(
+    ord("A")
+)  # Outputs 65 -> integer representing the Unicode code point of that character.
+
+print(1 < 2 < 3 < 4)  # True
+print(not (True))  # False
+
+
+# * is vs ==
+print(True == 1)  # True
+print("" == 1)  # False
+print([] == 1)  # False
+print(10 == 10.0)  # True
+print([] == [])  # True
+print()
+
+print(True == True)  # True
+print("1" == 1)  # False
+print(int("1") == 1)  # True
+print([1, 2, 3] == [1, 2, 3])  # True
+print()
+
+print(True == 1)  # False
+print("1" == 1)  # False
+print([] == 1)  # False
+print(10 == 10.0)  # False
+print([1, 2, 3] == [1, 2, 3])  # False
+print()
+
+print(True is True)  # True
+print("1" == "1")  # True
+print([] == [])  # False
