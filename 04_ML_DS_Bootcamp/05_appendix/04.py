2aeabce8a2956b5255876a796e2df684d02e4cdc
Michal Radomski <michalra@op.pl>
2026-02-23 22:35:46 +0100

    Machine Learning & Data Science Bootcamp - commit 67

--- 04_ML_DS_Bootcamp/05_appendix/04.py
+++ 04_ML_DS_Bootcamp/05_appendix/04.py
@@ -65,3 +65,118 @@ for item in my_list:
     counter += item
 
 print(counter)  # 55
+
+
+# * Range -> stop is omitted!
+print(range(100))  # range(0, 100)
+print(list(range(100)))  # [0, 1, ..., 99]
+
+for _ in range(1, 5):
+    print(_)  # 1 2 3 4
+
+print("\n")
+for _ in range(1, 10, 2):
+    print(_)  # 1 3 5 7 9
+
+print("\n")
+for _ in range(10, 0, -1):
+    print(_)  # 10 ... 1
+
+
+# * Enumerate()
+for i, char in enumerate("Hello"):
+    print(i, char)
+# 0 H
+# 1 e
+# 2 l
+# 3 l
+# 4 o
+
+for i, char in enumerate([1, 2, 3]):
+    print(i, char)
+
+for i, char in enumerate(range(100)):
+    if char == 50:
+        print(f"index of 50 is: {i}")
+
+
+# * While loops
+i = 0
+while i < 50:
+    print(i, end="\t")
+    i += 1
+else:
+    print("\nDone with all the work.")
+print()
+
+my_list = [1, 2, 3]
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+# 1
+# 2
+# 3
+print()
+
+while True:
+    response = input("Say Something: ")
+    if response == "bye":
+        break
+
+
+# * Break, Continue, Pass
+my_list = [1, 2, 3]
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+    continue
+print()
+# 1
+# 2
+# 3
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+    break
+print()
+# 1
+
+i = 0
+while i < len(my_list):
+    i += 1
+    pass  # Does nothing!
+
+print("No error")  # No error
+
+
+# * Exercise!
+# * Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+]
+
+# Answer:
+for row in picture:
+    for pixel in row:
+        if pixel:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print("")
+#    *
+#   ***
+#  *****
+# *******
+#    *
+#    *
+#    *
