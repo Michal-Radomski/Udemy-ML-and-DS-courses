c626d1d28c5e9b8bffe96d72883383588eed76ef
Michal Radomski <michalra@op.pl>
2026-03-03 00:24:14 +0100

    Pytorch course - commit 07

--- 07_Pytorch/model.py
+++ 07_Pytorch/model.py
@@ -1,6 +1,7 @@
 import pytorch_lightning as L  # type: ignore[import-not-found]
 import torch  # type: ignore[import-not-found]
 import torch.nn as nn  # type: ignore[import-not-found]
+import torch.optim as optim  # type: ignore[import-not-found]
 from data import CharTokenizer
 
 
@@ -20,12 +21,42 @@ class Generator(L.LightningModule):
         out = self.out_layer(rnn_out)
         return out, hidden
 
+    def prepend_bos(self, batch):
+        bs = batch.shape[0]
+        bos_tokens = torch.full((bs, 1), self.tokenizer.bos_token, device=batch.device)
+        output = torch.cat((bos_tokens, batch), dim=1)[:, :-1]
+        return output
+
+    def training_step(self, batch, batch_idx):
+        inp = self.prepend_bos(batch)
+        out, _ = self(inp)
+        loss = self.loss_fn(out.transpose(2, 1), batch)
+        self.log("loss", loss, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        pass
+
+    def test_step(self, batch, batch_idx):
+        pass
+
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr=1e-4)
+
 
 if __name__ == "__main__":
+    from data import CharDataModule, load_bookcorpus_data
+
+    data = load_bookcorpus_data()
+
     tokenizer = CharTokenizer.load("tokenizer.json")
-    generator = Generator(tokenizer.vocab_size, 4, 5, tokenizer)
+    datamodule = CharDataModule(data, tokenizer)
 
-    fake_batch = torch.randint(0, tokenizer.vocab_size - 1, (3, 17))
+    generator = Generator(tokenizer.vocab_size, 128, 512, tokenizer)
+    trainer = L.Trainer()
 
-    out = generator(fake_batch)
-    print(out)
+    trainer.fit(model=generator, datamodule=datamodule)
+
+    # fake_batch = torch.randint(0, tokenizer.vocab_size - 1, (3, 17))
+    # out = generator(fake_batch)
+    # print(out)
